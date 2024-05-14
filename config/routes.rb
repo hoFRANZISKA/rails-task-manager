6d2cdf892b570f3685c3add3cbf692e8606fde16
Franziska Horn <Franziska_Horn@hotmail.com>
2024-05-14 16:51:50 +0200

    Finalize task without styling or optional

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
   get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 
   # Defines the root path route ("/")
   # root "posts#index"
